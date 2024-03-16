75d158307b1d9e5a9e5cc491828f875634b8e991
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-03-16 13:48:45 +0530

    numbers basic

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#x=int(input("enter your first number"));
+#y=int(input("enter second number"));
+
+
+x=float(input("enter your first number"));
+y=float(input("enter second number"));
+z=round(x+y);
+
+a=round(x/y,2);
+
+print(a);
+#number formating
+print(f"{z:,}");
