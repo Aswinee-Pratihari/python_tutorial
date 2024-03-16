d2701a29795bcfb0dcd94a0fe6d1359555b0fb7f
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-03-16 21:32:49 +0530

    match (switch statements)

--- conditional.py
+++ conditional.py
@@ -1,40 +1,12 @@
-# score=int(input("Enter your score "))
+name=input("Enter your name")
 
-#method -1
-"""
-if(score>90 and score<=100):
-    print("Grade A")
-elif(score>60 and score<=90 ):
-    print("Grade B")
-elif(score>100 or score<0):
-    print("invalid score")
-else:
-    print("Grade C")
-"""
+match name:
+    case "Virat" | "Rohit" | "Gautam":
+        print("You are cricketer")
+    case "Chetri":
+        print("You are footballer")
+    case "Sania":
+        print("You are badminton player")
+    case _:
+        print("Who?")
 
-#method -2
-"""
-if(90<score<=100):
-    print("Grade A")
-elif(60<score<=90 ):
-    print("Grade B")
-elif(score>100 or score<0):
-    print("invalid score")
-else:
-    print("Grade C")
-"""
-
-
-# even odd number
-def main():
-    x=int(input("enter your number"))
-    if(isEven(x)):
-        print("number is even")
-    else:
-        print("number is odd")
-
-
-def isEven(x):
-   # return True if x%2==0 else False ;
-    return x%2==0;
-main() 
