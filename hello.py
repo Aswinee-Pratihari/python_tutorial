4b9bd81c72ed05f590bdc1247ed1301d859bf4b6
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-03-16 12:31:45 +0530

    strings basic

--- hello.py
+++ hello.py
@@ -1,2 +1,20 @@
 
-print("hello")
+name=input("what is your name ")
+
+#remove white spaces from str
+name=name.strip()
+
+#Capitalize name of user
+name=name.capitalize()
+
+#Capitalize first letter of each word
+name=name.title()
+
+print("hello " + name)
+
+# print("hello", name,name); #multiple argumrnts
+
+print("hello \"sir\""); #escape charecters (using double quotation inside double quotation)
+
+print(f"hello {name}")  #using variables in a string without concatination
+
