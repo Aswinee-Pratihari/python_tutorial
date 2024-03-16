c3c2ec6850623af3fda0f8869863dc6867cc80f3
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-03-16 20:58:50 +0530

    conditionals basic 01

---
+++ conditional.py
@@ -0,0 +1,24 @@
+x=int(input("Enter your first number "))
+y=int(input("Enter your second number "))
+
+""""
+if (x>y):
+    print("x is greater than y")
+elif x<y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+"""
+
+"""
+if(x<y or x>y):
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+"""
+
+
+if(x!=y):
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
