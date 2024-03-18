7d260b9d0ca4090bfddf51232ab2b4263fdfe218
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-03-18 10:46:22 +0530

    loop basics -03

--- loop.py
+++ loop.py
@@ -1,35 +1,58 @@
-"""
-while True:
-    n=int(input("what is value of n? "))
-    if n>0:
-        break;
+#dict
 
-for i in range(n):
-    print("hi "+ str(i+1) );
-"""
+students={"Akash":"Green",
+          "Harry":"Red",
+          "Rakesh":"Purple","Rohan":"White"}
 
-"""
-def main():
-    number=getNumber()
-    meow(number)
+house=["Green","Red","Purple","White"]
 
-def getNumber():
-    while True:
-        n=int(input("what is value of n? "))
-        if n>0:
-            return n;
-
-def meow(n):
-    for _ in range(n):
-        print("meow")
-main()
-"""
-
-#LIST/ARRAY
-students=["Akash","Harry","Rakesh","Rohan"]
+print(students["Rakesh"])
 
 for student in students:
-    print(student)
+   # print(student) #give all th keys
+    print(student,students[student],sep=",")
 
-for i in range(len(students)):
-    print(i+1,students[i])
+
+#dictionary within a list 
+students=[
+    {
+        "name":"Akash",
+        "House":"Green",
+        "class":"6"
+    },
+    {
+        "name":"Rahul",
+        "House":"Red",
+        "class":"8"
+    },
+    {
+        "name":"Rajesh",
+        "House":"White",
+        "class":"7"
+    },
+    {
+        "name":"Mehul",
+        "House":None,
+        "class":"10"
+    },
+    
+]
+
+for student in students:
+    print(student["name"],student["House"],student["class"] ,sep=",")
+
+
+#printing a 2d matrix
+
+# method-1
+#for each row in matrix 
+for i in range(3):
+        # for each column ->
+        for j in range(3):
+                print("#",end="")
+        print()
+
+
+#method-2
+for i in range(3):
+        print("*"*3)    
