92487916272b3c8afc4447ca02e387a785b9ff74
Aswinee-Pratihari <aswineepratihari010@gmail.com>
2024-03-18 09:56:07 +0530

    loops basic-01

---
+++ loop.py
@@ -0,0 +1,14 @@
+i=3;
+
+while(i!=0):
+    print("hello")
+    i-=1;
+
+for i in [1,2,3]:
+    print("hey")
+
+for _ in range(3): #i replaced with _ means we dont need the variable
+    print("hi sir")
+
+print("meow \n"*3,end="")
+
